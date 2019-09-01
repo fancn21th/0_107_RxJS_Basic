d44ec8aa717cbe7e1ee5044c075d67ed91bdfa77
fancn <fanyj@yikeer.com>
2019-09-01 12:09:19 +0800

    tryout:  interval

--- src/app/main.js
+++ src/app/main.js
@@ -1,1 +1,7 @@
-console.log("hello world!");
+console.clear();
+
+var source = Rx.Observable.interval(400)
+  .take(9)
+  .map(i => [1, 2, "foo", "bar", 1, 2, "foo", "bar", 1, 2, "foo", "bar"][i]);
+
+source.subscribe(x => console.log(x));
