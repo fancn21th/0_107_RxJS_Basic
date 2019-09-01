4a7f9a8301ffa2654bf9f6b92f0d6768577d1446
fancn <fanyj@yikeer.com>
2019-09-01 21:47:20 +0800

    feature: refresh stream

--- src/app/request-stream.js
+++ src/app/request-stream.js
@@ -1,30 +1,45 @@
 var refreshButton = document.querySelector(".refresh");
 
-var requestStream = Rx.Observable.of("https://api.github.com/users");
+var refreshClickStream = Rx.Observable.fromEvent(refreshButton, "click");
+
+var startupRequestStream = Rx.Observable.of("https://api.github.com/users");
+
+var requestOnRefreshStream = refreshClickStream.map(ev => {
+  var randomOffset = Math.floor(Math.random() * 500);
+  return "https://api.github.com/users?since=" + randomOffset;
+});
 
 // flatMap is now an alias for mergeMap
 // but will work just the same.
-var responseStream = requestStream.flatMap(requestUrl =>
-  Rx.Observable.fromPromise(jQuery.getJSON(requestUrl))
-);
+var responseStream = startupRequestStream
+  .merge(requestOnRefreshStream)
+  .flatMap(requestUrl => Rx.Observable.fromPromise(jQuery.getJSON(requestUrl)));
 
 function createSuggestionStream(responseStream) {
-  return responseStream.map(
-    listUser => listUser[Math.floor(Math.random() * listUser.length)]
-  );
+  return responseStream
+    .map(listUser => listUser[Math.floor(Math.random() * listUser.length)])
+    .startWith(null)
+    .merge(refreshClickStream.map(ev => null));
 }
 
 var suggestion1Stream = createSuggestionStream(responseStream);
 var suggestion2Stream = createSuggestionStream(responseStream);
 var suggestion3Stream = createSuggestionStream(responseStream);
 
-function renderSuggestion(userData, selector) {
-  var element = document.querySelector(selector);
-  var usernameEl = element.querySelector(".username");
-  usernameEl.href = userData.html_url;
-  usernameEl.textContent = userData.login;
-  var imgEl = element.querySelector("img");
-  imgEl.src = userData.avatar_url;
+// Rendering ---------------------------------------------------
+function renderSuggestion(suggestedUser, selector) {
+  var suggestionEl = document.querySelector(selector);
+  if (suggestedUser === null) {
+    suggestionEl.style.visibility = "hidden";
+  } else {
+    suggestionEl.style.visibility = "visible";
+    var usernameEl = suggestionEl.querySelector(".username");
+    usernameEl.href = suggestedUser.html_url;
+    usernameEl.textContent = suggestedUser.login;
+    var imgEl = suggestionEl.querySelector("img");
+    imgEl.src = "";
+    imgEl.src = suggestedUser.avatar_url;
+  }
 }
 
 suggestion1Stream.subscribe(user => {
